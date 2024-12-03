16c2183ff46799fb6a6791bfbc4d24ff9bd74a62
Rehan <rehanrockzz9966313@gmail.com>
2024-12-03 12:15:18 +0530

    .

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
-
+  onSearch(event: any): void {
+    const searchTerm = event.target.value;
+    console.log('Search term:', searchTerm);
+    // Handle search logic (e.g., filtering or making API calls)
+  }
 }
